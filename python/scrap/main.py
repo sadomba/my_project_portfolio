c31b2c0815fdc2585a58b9c286a1e099f169f051
Marvelous Sadomba <114573251+sadomba@users.noreply.github.com>
2024-08-22 13:57:54 +0200

    Delete python directory

--- python/scrap/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication
-from ui import LoginPage
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    login_page = LoginPage()
-    login_page.show()
-    sys.exit(app.exec_())
